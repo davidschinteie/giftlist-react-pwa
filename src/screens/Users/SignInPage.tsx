348a4bc9d4533aa8bcbe6c1d3aca019716f081ad
davidschinteie <davidschinteie@gmail.com>
2023-01-11 17:21:12 +0200

    toggle visible password

--- src/screens/Users/SignInPage.tsx
+++ src/screens/Users/SignInPage.tsx
@@ -14,9 +14,11 @@ const SignInPage = () => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [visiblePassword, setVisiblePassword] = useState(false);
 
   const signIn = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    console.log(event.target);
     const user = await logInWithEmailAndPassword(email, password);
     if (user !== null) {
       login({
@@ -66,7 +68,7 @@ const SignInPage = () => {
                 />
               </div>
 
-              <div className="flex items-center mb-4 text-lg">
+              <div className="flex items-center relative mb-4 text-lg">
                 <svg
                   className="absolute ml-3 z-10 fill-slate-800"
                   viewBox="0 0 24 24"
@@ -76,11 +78,46 @@ const SignInPage = () => {
                 </svg>
                 <input
                   className="rounded-3xl border-none bg-white bg-opacity-50 px-6 py-2 pl-12 font-medium text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
-                  type="password"
+                  type={visiblePassword ? "text" : "password"}
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   placeholder="Password"
                 />
+                <button
+                  onClick={(event) => {
+                    event.preventDefault();
+                    setVisiblePassword(!visiblePassword);
+                  }}
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
+                >
+                  <svg
+                    className={`h-6 text-gray-700 ${
+                      visiblePassword ? "hidden" : "block"
+                    }`}
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"
+                    ></path>
+                  </svg>
+
+                  <svg
+                    className={`h-6 text-gray-700 ${
+                      visiblePassword ? "block" : "hidden"
+                    }`}
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 640 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 0 52.87-4 77.89-10.46L346 397.39a144.13 144.13 0 0 1-26 2.61zm313.82 58.1l-110.55-85.44a331.25 331.25 0 0 0 81.25-102.07 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45zm-183.72-142l-39.3-30.38A94.75 94.75 0 0 0 416 256a94.76 94.76 0 0 0-121.31-92.21A47.65 47.65 0 0 1 304 192a46.64 46.64 0 0 1-1.54 10l-73.61-56.89A142.31 142.31 0 0 1 320 112a143.92 143.92 0 0 1 144 144c0 21.63-5.29 41.79-13.9 60.11z"
+                    ></path>
+                  </svg>
+                </button>
               </div>
 
               <div className="mt-8 flex justify-center text-lg text-black">
